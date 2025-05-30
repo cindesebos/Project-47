84bcc964aa01c1fd8010d9119bcdd968acbaf7be
cindesebos <y07programmer@gmail.com>
2025-05-30 16:59:48 +0200

    Сейс коммит

--- Assets/Users/cindesebos/Scripts/Character/CharacterItemProvider.cs
+++ Assets/Users/cindesebos/Scripts/Character/CharacterItemProvider.cs
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 using System;
+=======
+using UnityEngine;
+using Scripts.Items;
+>>>>>>> Stashed changes
 using Scripts.Character.Inventory;
 using Scripts.Items;
 using Scripts.UI;
@@ -18,21 +23,32 @@ namespace Scripts.Character
         private ArtsToggler _artsToggler;
 
         private Item _currentSelectedItem;
+<<<<<<< Updated upstream
         public Note _currentSelectedNote;
         private RaycastHit _lastHit;
         private bool _hasHit;
 
         public void Initialize(Transform cameraOrigin, CharacterData data, IInventory inventory, ArtsToggler artsToggler)
+=======
+        private RaycastHit _lastHit;
+        private bool _hasHit;
+
+        public void Initialize(Transform cameraOrigin, CharacterData data, IInventory inventory)
+>>>>>>> Stashed changes
         {
             _cameraOrigin = cameraOrigin;
             _rayDistance = data.RayDistance;
             _targetLayer = data.TargetLayer;
             _inventory = inventory;
+<<<<<<< Updated upstream
             _artsToggler = artsToggler;
+=======
+>>>>>>> Stashed changes
         }
 
         public void UseItem(InputAction.CallbackContext context)
         {
+<<<<<<< Updated upstream
             if (_currentSelectedItem)
             {
                 if (_inventory.TryAddItem(_currentSelectedItem.Data))
@@ -52,6 +68,14 @@ namespace Scripts.Character
                 _artsToggler.Show(_currentSelectedNote.Data.Sprite);
 
                 _currentSelectedNote = null;
+=======
+            if (_currentSelectedItem == null) return;
+
+            if (_inventory.TryAddItem(_currentSelectedItem.Data))
+            {
+                Destroy(_currentSelectedItem.gameObject);
+                _currentSelectedItem = null;
+>>>>>>> Stashed changes
             }
         }
 
@@ -59,16 +83,22 @@ namespace Scripts.Character
         {
             if (_currentSelectedItem != null)
                 _currentSelectedItem.SetOutlineVisible(false);
+<<<<<<< Updated upstream
 
             if (_currentSelectedNote != null)
                 _currentSelectedNote.SetOutlineVisible(false);
 
+=======
+
+            _currentSelectedItem = null;
+>>>>>>> Stashed changes
             _hasHit = false;
 
             var ray = new Ray(_cameraOrigin.position, _cameraOrigin.forward);
 
             if (Physics.Raycast(ray, out RaycastHit hit, _rayDistance, _targetLayer))
             {
+<<<<<<< Updated upstream
                 if (hit.collider.GetComponent<ISelectable>() != null)
                 {
                     if (hit.collider.TryGetComponent(out Item item))
@@ -105,6 +135,26 @@ namespace Scripts.Character
         {
             if (_cameraOrigin == null)
                 return;
+=======
+                Debug.Log($"Hit: {hit.collider.name} at {hit.point}");
+
+                if (hit.collider.TryGetComponent(out Item item))
+                {
+                    Debug.Log($"Item found: {item.Data.Name}");
+
+                    _currentSelectedItem = item;
+                    _currentSelectedItem.SetOutlineVisible(true);
+                    _lastHit = hit;
+                    _hasHit = true;
+                }
+            }
+        }
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (_cameraOrigin == null) return;
+>>>>>>> Stashed changes
 
             Gizmos.color = Color.cyan;
             Vector3 rayStart = _cameraOrigin.position;
@@ -118,11 +168,14 @@ namespace Scripts.Character
                 Gizmos.DrawSphere(_lastHit.point, 0.1f);
             }
         }
+<<<<<<< Updated upstream
 
         internal void Initialize(Transform transform, CharacterData data, IInventory inventory)
         {
             throw new NotImplementedException();
         }
+=======
+>>>>>>> Stashed changes
 #endif
     }
 }
